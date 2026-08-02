d753b23291833a92c1de13c091f989efe53f83b3
Dev leo <devleothedamian@gmail.com>
2026-08-02 16:53:10 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+export default {
+    nombre: 'meowl',
+    nombre2: 'meowl bot',
+    prefix: '#',
+    banner: 'https://raw.githubusercontent.com/JTxs00/db/main/1785685103656.jpeg',
+    botowner: '5492645746772',
+    owner: ['5492645576493', '542645576493', '5219992042946'],
+    version: '1.0.4',
+    tipo: '🅟rincipal Ⓥ',
+    creador: '—͟͞Dev leo 𒆜ᴼᶠⁱᶜⁱᵃˡ',
+    canalId: '120363409173174836@newsletter',
+    canalNombre: 'MEOWL ULTRA BOT',
+};
